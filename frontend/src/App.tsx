c0b84aa8a1d8667c1e41736a8608bcadd3967db8
Medvedeva Valentina <72570836+medvalna@users.noreply.github.com>
2024-03-15 14:35:44 +0300

    fix: landing mob version

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Cover from "./assets/Cover.png";
+import Cover_mob from "./assets/Cover_mob.png";
 function App() {
   return (
     <>
@@ -7,6 +8,7 @@ function App() {
         <div className="layout">
           <div className="cover_container">
             <img src={Cover} className="cover" alt="Image" />
+            <img src={Cover_mob} className="cover_mob" alt="Image" />
           </div>
           <button className="button">
             <span className="button-text">Login</span>
@@ -16,30 +18,29 @@ function App() {
             <h3>best app for creating your forms</h3>
           </div>
         </div>
+
         <h2 className="question-title"> What Can You Do?</h2>
-        <div className="pros">
-          <div className="points">
-            <div>
-              <h3>Create form</h3>
-              <h4>
-                Effortlessly create your new form with our powerful and
-                intuitive form app
-              </h4>
-            </div>
-            <div>
-              <h3>Answer on forms</h3>
-              <h4>
-                Discover insightful critiques and share your thoughts on diverse
-                literary masterpieces effortlessly
-              </h4>
-            </div>
-            <div>
-              <h3>See results</h3>
-              <h4>
-                Easily visualize your form submission data with our built-in
-                analytics tools
-              </h4>
-            </div>
+        <div className="points">
+          <div className="pros">
+            <h3>Create form</h3>
+            <h4>
+              Effortlessly create your new form with our powerful and intuitive
+              form app
+            </h4>
+          </div>
+          <div className="pros">
+            <h3>Answer on forms</h3>
+            <h4>
+              Discover insightful critiques and share your thoughts on diverse
+              literary masterpieces effortlessly
+            </h4>
+          </div>
+          <div className="pros">
+            <h3>See results</h3>
+            <h4>
+              Easily visualize your form submission data with our built-in
+              analytics tools
+            </h4>
           </div>
         </div>
       </div>
