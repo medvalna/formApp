9decd9975ed6223785760f07c8c119a9d580964f
Medvedeva Valentina <72570836+medvalna@users.noreply.github.com>
2024-03-16 11:57:56 +0300

    feaat: login page

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
 // https://vitejs.dev/config/
 export default defineConfig({
+  root,
   plugins: [react()],
-})
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        login: resolve(root, "login", "index.html"),
+      },
+    },
+  },
+});
