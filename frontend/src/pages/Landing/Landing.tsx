76c4a17ae56d285c2aa56fdeed82eedc7d65130b
Medvedeva Valentina <72570836+medvalna@users.noreply.github.com>
2024-03-20 13:43:22 +0300

    fix: move landing page to page dir

---
+++ frontend/src/pages/Landing/Landing.tsx
@@ -0,0 +1,57 @@
+import "./Landing.css";
+import Cover from "../../assets/Cover.png";
+import Cover_mob from "../../assets/Cover_mob.png";
+import IconPlus from "../../assets/IconPlus.png";
+import IconMessage from "../../assets/IconMessage.png";
+import IconFile from "../../assets/IconFile.png";
+export function Landing() {
+  return (
+    <>
+      <div className="box">
+        <div className="up_part">
+          <div className="cover_container">
+            <img src={Cover} className="cover" alt="Image" />
+            <img src={Cover_mob} className="cover_mob" alt="Image" />
+          </div>
+          <button className="button">
+            <span className="button-text">Login</span>
+          </button>
+          <div className="title">
+            <h1>Form App</h1>
+            <h3>best app for creating your forms</h3>
+          </div>
+        </div>
+
+        <h2 className="question-title"> What Can You Do?</h2>
+        <div className="points">
+          <div className="pros">
+            <img src={IconPlus} className="icon" alt="Image" />
+            <h3 className="h3title">Create form</h3>
+            <h4>
+              Effortlessly create your new form with our powerful and intuitive
+              form app
+            </h4>
+          </div>
+          <div className="pros">
+            <img src={IconMessage} className="icon" alt="Image" />
+            <h3 className="h3title">Answer on forms</h3>
+            <h4>
+              Discover insightful critiques and share your thoughts on diverse
+              literary masterpieces effortlessly
+            </h4>
+          </div>
+          <div className="pros">
+            <img src={IconFile} className="icon" alt="Image" />
+            <h3 className="h3title">See results</h3>
+            <h4>
+              Easily visualize your form submission data with our built-in
+              analytics tools
+            </h4>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Landing;
