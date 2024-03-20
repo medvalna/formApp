bfe73a344b9d5f4298b29ca6c0c165f6f2b2ac71
Medvedeva Valentina <72570836+medvalna@users.noreply.github.com>
2024-03-20 13:44:12 +0300

    feat: added router

---
+++ frontend/src/components/Router/Router.tsx
@@ -0,0 +1,18 @@
+﻿import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import App from "../App/App";
+import { SignUp } from "../../pages/SignUp/SignUp";
+import { Landing } from "../../pages/Landing/Landing";
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />} path="/">
+          <Route element={<Landing />} path="/" />
+          <Route element={<SignUp />} path="/signUp" />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
