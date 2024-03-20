a35864d3df8aa98c6bf4e8d4f46b8575d9384144
Medvedeva Valentina <72570836+medvalna@users.noreply.github.com>
2024-03-20 13:43:06 +0300

    feat: added sign up page

---
+++ frontend/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,34 @@
+﻿import "./SignUp.css";
+import { TextField } from "@radix-ui/themes";
+import "../../index.css";
+export function SignUp() {
+  return (
+    <>
+      <div className="layout">
+        <div className="placing">
+          <h4 className="title_signup">Sign up</h4>
+          <div className="inputs">
+            <div>
+              <TextField.Input
+                color="indigo"
+                className="input"
+                size="2"
+                placeholder="Email"
+              />
+            </div>
+            <div>
+              <TextField.Input
+                className="input"
+                size="3"
+                placeholder="Password"
+              />
+            </div>
+            <button className="button_su">
+              <span className="button-text_su">Login</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
