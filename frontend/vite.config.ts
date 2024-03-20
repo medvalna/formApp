166c35610b636eee570cb5fe1418307e901c40f7
Medvedeva Valentina <72570836+medvalna@users.noreply.github.com>
2024-03-20 13:48:47 +0300

    fix: delete unused vite config stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
-// https://vitejs.dev/config/
 export default defineConfig({
   root,
   plugins: [react()],
@@ -13,8 +12,7 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(root, "index.html"),
-        login: resolve(root, "login", "index.html"),
+        main: resolve(root, `index.html`),
       },
     },
   },
