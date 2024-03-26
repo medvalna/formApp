b8b1fd5b006d97a7a2cc437b8617622e86290ab8
Medvedeva Valentina <72570836+medvalna@users.noreply.github.com>
2024-03-26 16:21:32 +0300

    fix: added usage of auth hook to homepage

--- frontend/src/pages/Homepage/Homepage.tsx
+++ frontend/src/pages/Homepage/Homepage.tsx
@@ -1,8 +1,19 @@
-﻿import "../../index.css";
+﻿import { useNavigate } from "react-router-dom";
+import "../../index.css";
+import { useEffect } from "react";
+import { useMe } from "../../hooks/useMe";
+
 export function Homepage() {
+  const navigator = useNavigate();
+  const isAuth = useMe();
+  useEffect(() => {
+    if (!isAuth) {
+      navigator("/sign-up");
+    }
+  }, [isAuth]);
   return (
-    <>
+    <div>
       <h1>Homepage!</h1>
-    </>
+    </div>
   );
 }
